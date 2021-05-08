461ea43bc73035569525c42da6fba25f1155cc40
Sjuul Janssen <sjuul@outlook.com>
2021-05-08 19:35:22 +0200

    adding access to jobs and cronjobs

--- controllers/dbserver_controller.go
+++ controllers/dbserver_controller.go
@@ -42,6 +42,8 @@ type DbServerReconciler struct {
 // Generic Kubebuilder rules:
 // +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch
 // +kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=batch/v1beta,resources=cronjobs,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=batch/v1,resources=jobs,verbs=get;list;watch;create;update;patch;delete
 
 func (r *DbServerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = r.Log.WithValues("dbserver", req.NamespacedName)
